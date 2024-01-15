fda8a9b77975a5acc8f899fc56bf01f6b41197c8
acs0209 <annjae4676@naver.com>
2024-01-16 00:59:28 +0900

    feat: 메시지 전송 브로드캐스팅 구현

--- src/backend/chat-service/src/main/java/harmony/chatservice/controller/CommunityMessageController.java
+++ src/backend/chat-service/src/main/java/harmony/chatservice/controller/CommunityMessageController.java
@@ -3,6 +3,7 @@ package harmony.chatservice.controller;
 import harmony.chatservice.dto.CommunityMessageDto;
 import harmony.chatservice.dto.request.CommunityMessageRequest;
 import harmony.chatservice.service.CommunityMessageService;
+import harmony.chatservice.service.kafka.MessageProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommunityMessageController {
 
     private final CommunityMessageService messageService;
-
+    private final MessageProducerService messageProducerService;
 
     @MessageMapping("/guild/message")
     public void chatMessage(CommunityMessageRequest messageRequest) {
         CommunityMessageDto messageDto = messageService.saveMessage(messageRequest);
+        messageProducerService.sendMessageForCommunity(messageDto);
     }
 }
