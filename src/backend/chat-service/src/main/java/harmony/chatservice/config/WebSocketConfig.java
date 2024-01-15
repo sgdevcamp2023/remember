b356c2e849ec365a95febd34c6a91b56cd4e2f6e
acs0209 <annjae4676@naver.com>
2024-01-16 00:14:39 +0900

    feat: WebSocket 활성화와 메시지 브로커 설정 구현

---
+++ src/backend/chat-service/src/main/java/harmony/chatservice/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package harmony.chatservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // 특정 topic을 구독, 즉 구독, 발행 경로를 설정한다.
+        config.setApplicationDestinationPrefixes("/api/chat"); // stomp를 사용하여 오는 경로와 일반 rest api 경로를 구분하기 위해 prefix를 설정
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
