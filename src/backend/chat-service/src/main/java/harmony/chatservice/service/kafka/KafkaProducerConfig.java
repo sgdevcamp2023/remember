f9c0f737e80656e501830855d758a639dff193e6
acs0209 <annjae4676@naver.com>
2024-01-16 00:42:52 +0900

    feat: 카프카 producer 설정 구현

---
+++ src/backend/chat-service/src/main/java/harmony/chatservice/service/kafka/KafkaProducerConfig.java
@@ -0,0 +1,55 @@
+package harmony.chatservice.service.kafka;
+
+import harmony.chatservice.dto.CommunityMessageDto;
+import harmony.chatservice.dto.DirectMessageDto;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return config;
+    }
+
+    // 커뮤니티 관련 producer
+    public ProducerFactory<String, CommunityMessageDto> producerFactoryForCommunity() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    // 커뮤니티 관련 producer
+    @Bean
+    public KafkaTemplate<String, CommunityMessageDto> kafkaTemplateForCommunity() {
+        return new KafkaTemplate<>(producerFactoryForCommunity());
+    }
+
+    // 다이렉트 관련 producer
+    public ProducerFactory<String, DirectMessageDto> producerFactoryForDirect() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    // 다이렉트 관련 producer
+    @Bean
+    public KafkaTemplate<String, DirectMessageDto> kafkaTemplateForDirect() {
+        return new KafkaTemplate<>(producerFactoryForDirect());
+    }
+}
