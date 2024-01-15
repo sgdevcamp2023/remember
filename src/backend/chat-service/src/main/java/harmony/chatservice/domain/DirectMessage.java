1777fc9e2b16da1eef87a8673fe10dac73306c64
acs0209 <annjae4676@naver.com>
2024-01-15 23:26:08 +0900

    feat: direct message 도메인 구현

---
+++ src/backend/chat-service/src/main/java/harmony/chatservice/domain/DirectMessage.java
@@ -0,0 +1,72 @@
+package harmony.chatservice.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "directMessages")
+public class DirectMessage extends BaseModel{
+
+    @Transient
+    public static final String SEQUENCE_NAME = "directMessages_sequence";
+
+    @Id
+    private Long messageId;
+
+    @Field
+    private Long roomId;
+
+    @Field
+    private Long parentId;
+
+    @Field
+    private Long userId;
+
+    @Field
+    private String profileImage;
+
+    @Field
+    private String type;
+
+    @Field
+    private String senderName;
+
+    @Field
+    private String message;
+
+    @Field
+    private boolean delCheck;
+
+    @Builder
+    public DirectMessage(Long roomId, Long parentId, Long userId, String profileImage, String type, String senderName, String message) {
+        this.roomId = roomId;
+        this.parentId = parentId;
+        this.userId = userId;
+        this.profileImage =profileImage;
+        this.type =type;
+        this.senderName = senderName;
+        this.message = message;
+        this.delCheck = false;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass{" +
+                "messageId='" + messageId + '\'' +
+                "roomId='" + roomId + '\'' +
+                "profileImage='" + profileImage + '\'' +
+                "type='" + type + '\'' +
+                "userId='" + userId + '\'' +
+                "senderName='" + senderName + '\'' +
+                "message=" + message +
+                '}';
+    }
+}
