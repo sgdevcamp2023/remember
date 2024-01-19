2b484922df339a4a4386266cecc01420b5b4b5d7
0Chord <seaweed.0chord@gmail.com>
2024-01-19 19:53:57 +0900

    feat : add INVALID_INPUT Exception handling

--- src/backend/community-service/src/main/java/harmony/communityservice/common/advice/AllControllerAdvice.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/common/advice/AllControllerAdvice.java
@@ -4,6 +4,7 @@ import harmony.communityservice.common.dto.BaseExceptionResponse;
 import harmony.communityservice.common.dto.BaseResponse;
 import harmony.communityservice.common.exception.NotFoundDataException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +15,12 @@ public class AllControllerAdvice {
     public BaseResponse<?> exceptionHandler(NotFoundDataException e) {
         return new BaseResponse<>(HttpStatus.BAD_REQUEST.value(), "BAD_REQUEST",
                 new BaseExceptionResponse("NOT_FOUND_DATA", 5000, "해당하는 데이터를 찾을 수 없습니다."));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public BaseResponse<BaseExceptionResponse> exceptionHandler(HttpRequestMethodNotSupportedException e) {
+        return new BaseResponse<>(HttpStatus.BAD_REQUEST.value(), "BAD_REQUEST", new BaseExceptionResponse(
+                "INVALID_INPUT", 5001, "잘못된 입력입니다."
+        ));
     }
 }
