006383dd490fdc6b9aea696d98f8d27d7904beab
acs0209 <annjae4676@naver.com>
2024-01-22 13:47:56 +0900

    feat: 파일 업로드 용량 예외 처리 구현

--- src/backend/chat-service/src/main/java/harmony/chatservice/exception/ExceptionStatus.java
+++ src/backend/chat-service/src/main/java/harmony/chatservice/exception/ExceptionStatus.java
@@ -7,7 +7,9 @@ public enum ExceptionStatus {
 
     DATA_NOT_FOUND(2004,  "Not Found", "삭제 또는 수정하려는 데이터를 찾을 수 없습니다."),
 
-    FILE_UPLOAD_EXCEEDED(2005,  "File Count Error", "파일 업로드 개수는 10개 이하여야 합니다.");
+    FILE_UPLOAD_EXCEEDED(2005,  "File Count Error", "파일 업로드 개수는 10개 이하여야 합니다."),
+
+    FILE_SIZE_EXCEED(2006, "File Size Error","업로드 할 수 있는 파일의 최대 크기는 15MB 입니다.");
 
     private final int statusCode;
     private final String message;
--- src/backend/chat-service/src/main/java/harmony/chatservice/exception/advice/GlobalExceptionHandler.java
+++ src/backend/chat-service/src/main/java/harmony/chatservice/exception/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package harmony.chatservice.exception.advice;
 
 import harmony.chatservice.exception.DataNotFoundException;
 import harmony.chatservice.exception.ExceptionResponse;
+import harmony.chatservice.exception.ExceptionStatus;
 import harmony.chatservice.exception.FileException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
@@ -33,5 +35,16 @@ public class GlobalExceptionHandler {
         return new ExceptionResponse(e.getExceptionStatus().getStatusCode(),
                 e.getExceptionStatus().getMessage(),
                 e.getExceptionStatus().getDesc());
+    }
+
+    /**
+     * 파일 업로드 용량 초과시 발생
+     */
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    protected ExceptionResponse handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+
+        return new ExceptionResponse(ExceptionStatus.FILE_UPLOAD_EXCEEDED.getStatusCode(),
+                ExceptionStatus.FILE_UPLOAD_EXCEEDED.getMessage(),
+                ExceptionStatus.FILE_UPLOAD_EXCEEDED.getDesc());
     }
 }
