2e8d59f1e8dbb2dc4a3286da42f1adfe5efe7bbb
0Chord <seaweed.0chord@gmail.com>
2024-01-23 14:55:40 +0900

    feat : add /api/community/check/room/guild/{user_id} api

--- src/backend/community-service/src/main/java/harmony/communityservice/community/query/controller/InnerApiQueryController.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/community/query/controller/InnerApiQueryController.java
@@ -1,7 +1,10 @@
 package harmony.communityservice.community.query.controller;
 
 import harmony.communityservice.common.dto.BaseResponse;
+import harmony.communityservice.community.query.dto.RoomGuildResponseDto;
+import harmony.communityservice.community.query.service.impl.InnerApiQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/community")
-public class UserQueryController {
+public class InnerApiQueryController {
 
+    private final InnerApiQueryService innerApiQueryService;
 
     @GetMapping("/check/room/guild/{userId}")
     public BaseResponse<?> search(@PathVariable Long userId) {
-
+        RoomGuildResponseDto roomGuildResponseDto = innerApiQueryService.search(userId);
+        return new BaseResponse<>(HttpStatus.OK.value(), "OK", roomGuildResponseDto);
     }
 }
--- src/backend/community-service/src/main/java/harmony/communityservice/community/query/service/impl/InnerApiQueryService.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/community/query/service/impl/InnerApiQueryService.java
@@ -1,2 +1,31 @@
-package harmony.communityservice.community.query.service.impl;public class InnerApiQueryService {
+package harmony.communityservice.community.query.service.impl;
+
+import harmony.communityservice.community.domain.GuildRead;
+import harmony.communityservice.community.query.dto.RoomGuildResponseDto;
+import harmony.communityservice.community.query.service.GuildReadQueryService;
+import harmony.communityservice.community.query.service.UserQueryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class InnerApiQueryService {
+
+    private final UserQueryService userQueryService;
+    private final GuildReadQueryService guildReadQueryService;
+
+    public RoomGuildResponseDto search(long userId) {
+        List<Long> roomIds = userQueryService.findUser(userId)
+                .getRoomUsers()
+                .stream()
+                .map(roomUser -> roomUser.getRoom().getRoomId()).toList();
+        List<Long> guildIds = guildReadQueryService.findGuildReadsByUserId(userId)
+                .stream()
+                .map(GuildRead::getGuildId).toList();
+        return new RoomGuildResponseDto(roomIds, guildIds);
+
+    }
 }
