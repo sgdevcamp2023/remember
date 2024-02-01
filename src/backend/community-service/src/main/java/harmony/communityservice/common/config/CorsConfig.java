b36b98f794eafbbcfb576bc1a422c558684cbdf2
0Chord <seaweed.0chord@gmail.com>
2024-02-01 13:33:18 +0900

    feat : add error logging handler

--- src/backend/community-service/src/main/java/harmony/communityservice/common/config/CorsConfig.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/common/config/CorsConfig.java
@@ -11,6 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://10.99.19.2:3000");
     }
 }
--- src/backend/community-service/src/main/java/harmony/communityservice/common/utils/LoggingUtils.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/common/utils/LoggingUtils.java
@@ -1,2 +1,25 @@
-package harmony.communityservice.common.utils;public class LoggingUtils {
+package harmony.communityservice.common.utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
+@Slf4j
+public class LoggingUtils {
+
+    public static void errorLogging(HttpServletRequest request, String exception) {
+        String apiAddr = request.getRequestURI();
+        String traceId = request.getHeader("trace-id");
+        String userId = request.getHeader("user-id");
+        String httpMethod = request.getMethod();
+        MDC.put("Trace-Id", traceId);
+        MDC.put("Api-Addr", apiAddr);
+        MDC.put("Http-Method", httpMethod);
+        MDC.put("User-Id", userId);
+        log.error(exception);
+        MDC.remove("Trace-Id");
+        MDC.remove("Api-Addr");
+        MDC.remove("Http-Method");
+        MDC.remove("User-Id");
+    }
 }
