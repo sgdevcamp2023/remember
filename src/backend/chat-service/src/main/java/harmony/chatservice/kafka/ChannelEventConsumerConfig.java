73cc3c1f7ee9403d6d8606e63c48915d1495b448
acs0209 <annjae4676@naver.com>
2024-02-04 13:09:34 +0900

    refactor: 컨슈머 설정 어노테이션 추가

--- src/backend/chat-service/src/main/java/harmony/chatservice/kafka/ChannelEventConsumerConfig.java
+++ src/backend/chat-service/src/main/java/harmony/chatservice/kafka/ChannelEventConsumerConfig.java
@@ -7,11 +7,15 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+@EnableKafka
+@Configuration
 public class ChannelEventConsumerConfig {
 
     @Value("${spring.kafka.consumer.bootstrap-servers}")
