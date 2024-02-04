fb1784a4e4afed95f171875a9271b2aae3349448
acs0209 <annjae4676@naver.com>
2024-02-04 20:56:47 +0900

    refactor: 상태 정보 키 변경

--- src/backend/state-service/stateservice/src/main/java/harmony/stateservice/service/ChatServerService.java
+++ src/backend/state-service/stateservice/src/main/java/harmony/stateservice/service/ChatServerService.java
@@ -1,8 +1,6 @@
 package harmony.stateservice.service;
 
 import harmony.stateservice.dto.SessionDto;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.HashOperations;
@@ -20,24 +18,21 @@ public class ChatServerService {
     public String updateSession(SessionDto sessionDto) {
         HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
         String sessionId = sessionDto.getSessionId();
-        Map<String, Object> map = new HashMap<>();
+        String stateKey = "USER:STATE";
         String userId = null;
         if (sessionDto.getType().equals("CONNECT")) {
             userId = String.valueOf(sessionDto.getUserId());
-            map.put("state", sessionDto.getState());
-            hashOperations.putAll(userId, map);
+            hashOperations.put(stateKey, userId, sessionDto.getState());
             saveSessionId(sessionId, userId);
         }
         if (sessionDto.getType().equals("DISCONNECT")) {
             userId = getUserId(sessionId);
-            map.put("state", sessionDto.getState());
-            hashOperations.putAll(userId, map);
+            hashOperations.put(stateKey, userId, sessionDto.getState());
             deleteSessionId(sessionId);
         }
 
         log.info("userId {}", userId);
-        log.info("sessionId {}", sessionDto.getSessionId());
-        log.info("user state {}", hashOperations.get(userId, "state"));
+        log.info("user state {}", hashOperations.get(stateKey, userId));
         return userId;
     }
 
