8f218d85bc1b024f080ef4d38e2c66a53654dfcf
HongStudy <fnvl7855@naver.com>
2024-02-19 21:42:21 +0900

    feat: BE/hjh

--- src/backend/api-gateway/SmileGatewayCore/Filter/DownStream/OriginalFilter.cs
+++ src/backend/api-gateway/SmileGatewayCore/Filter/DownStream/OriginalFilter.cs
@@ -8,18 +8,13 @@ internal class OriginalFilter : DownStreamFilter
 {
     protected override void Working(Adapter adapter, HttpContext context)
     {
-        context.Response.Header["Access-Control-Allow-Origin"] = context.Request.Header["Origin"];
-        if(context.Request.Header["Access-Control-Allow-Origin"].Contains("localhost"))
-        {
-            context.Response.Header["Access-Control-Allow-Origin"].Replace("localhost", "127.0.0.1");
-        }
-
         if (context.Request.Header.ContainsKey("Host"))
             context.Request.Header["Host"] = adapter.ListenerAddress;
     }
 
     protected override void Worked(Adapter adapter, HttpContext context)
     {
+        context.Response.Header["Access-Control-Allow-Origin"] = "*";
         // Origin을 확인해서, https인지 http인지 확인해서 넘겨줘야 할 듯?
         // context.Response.Header["Access-Control-Allow-Origin"] = $"https://localhost:3000";
     }
