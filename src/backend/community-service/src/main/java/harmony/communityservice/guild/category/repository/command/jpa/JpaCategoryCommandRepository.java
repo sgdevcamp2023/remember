58fa1bec52bf6a86158d491746196ae0cc6d3d5e
0Chord <seaweed.0chord@gmail.com>
2024-04-25 13:09:21 +0900

    refactor : category delete command method change bulk command

--- src/backend/community-service/src/main/java/harmony/communityservice/guild/category/repository/command/jpa/JpaCategoryCommandRepository.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/guild/category/repository/command/jpa/JpaCategoryCommandRepository.java
@@ -5,12 +5,17 @@ import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaCategoryCommandRepository extends JpaRepository<Category, Long> {
 
     void deleteCategoryByCategoryId(Long categoryId);
 
-    void deleteCategoriesByGuildId(Long guildId);
+    @Modifying
+    @Query("delete from Category c where c.guildId = :guildId")
+    void deleteCategoriesByGuildId(@Param("guildId") Long guildId);
 
     @Lock(value = LockModeType.PESSIMISTIC_READ)
     Optional<Category> findById(Long categoryId);
