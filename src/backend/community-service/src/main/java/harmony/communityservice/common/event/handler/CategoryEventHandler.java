a0273947feaad1ddb65694159a5e5bbcb99ce517
0Chord <seaweed.0chord@gmail.com>
2024-05-08 09:41:05 +0900

    feat : add inner category delete event transaction outbox pattern

--- src/backend/community-service/src/main/java/harmony/communityservice/common/event/handler/CategoryEventHandler.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/common/event/handler/CategoryEventHandler.java
@@ -1,6 +1,11 @@
 package harmony.communityservice.common.event.handler;
 
 import harmony.communityservice.common.event.dto.inner.DeleteCategoryEvent;
+import harmony.communityservice.common.exception.NotFoundDataException;
+import harmony.communityservice.common.outbox.InnerEventOutBoxMapper;
+import harmony.communityservice.common.outbox.InnerEventRecord;
+import harmony.communityservice.common.outbox.InnerEventType;
+import harmony.communityservice.common.outbox.SentType;
 import harmony.communityservice.guild.category.service.command.CategoryCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.retry.annotation.Backoff;
@@ -16,13 +21,36 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class CategoryEventHandler {
 
+    private final InnerEventOutBoxMapper outBoxMapper;
     private final CategoryCommandService categoryCommandService;
+
+    @TransactionalEventListener(classes = DeleteCategoryEvent.class, phase = TransactionPhase.BEFORE_COMMIT)
+    public void categoryDeleteEventBeforeHandler(DeleteCategoryEvent event) {
+        InnerEventRecord record = createCategoryDeleteEvent(event);
+        outBoxMapper.insertInnerEventRecord(record);
+    }
 
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Retryable(retryFor = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000L))
     @TransactionalEventListener(classes = DeleteCategoryEvent.class, phase = TransactionPhase.AFTER_COMMIT)
-    public void handler(DeleteCategoryEvent event) {
-        categoryCommandService.deleteByGuildId(event.guildId());
+    public void categoryDeleteEventAfterHandler(DeleteCategoryEvent event) {
+        InnerEventRecord record = createCategoryDeleteEvent(event);
+        InnerEventRecord innerEventRecord = outBoxMapper.findInnerEventRecord(record)
+                .orElseThrow(NotFoundDataException::new);
+        try {
+            categoryCommandService.deleteByGuildId(innerEventRecord.getGuildId());
+            outBoxMapper.updateInnerEventRecord(SentType.SEND_SUCCESS, innerEventRecord.getEventId());
+        } catch (Exception e) {
+            outBoxMapper.updateInnerEventRecord(SentType.SEND_FAIL, innerEventRecord.getEventId());
+        }
+    }
+
+    private InnerEventRecord createCategoryDeleteEvent(DeleteCategoryEvent event) {
+        return InnerEventRecord.builder()
+                .type(InnerEventType.DELETED_CATEGORY)
+                .sentType(SentType.INIT)
+                .guildId(event.guildId())
+                .build();
     }
 }
