07584a9ad5774d480b5585d8f14b398e746b3328
0Chord <seaweed.0chord@gmail.com>
2024-05-23 22:18:06 +0900

    test : add user unit test

---
+++ src/backend/community-service/src/test/java/harmony/communityservice/user/domain/CommonUserInfoTest.java
@@ -0,0 +1,42 @@
+package harmony.communityservice.user.domain;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CommonUserInfoTest {
+
+
+    @Test
+    @DisplayName("value가 모두 일치할 때 같은 객체로 인식 테스트")
+    void same_commonUserInfo() {
+        CommonUserInfo firstCommonUserInfo = CommonUserInfo.make("test", "https://cdn.com/test");
+        CommonUserInfo secondCommonUserInfo = CommonUserInfo.make("test", "https://cdn.com/test");
+        boolean equals = firstCommonUserInfo.equals(secondCommonUserInfo);
+
+        assertSame(equals, true);
+    }
+
+    @Test
+    @DisplayName("value가 다를 때 다른 객체로 인식 테스트")
+    void different_commonUserInfo() {
+        CommonUserInfo firstCommonUserInfo = CommonUserInfo.make("test1", "https://cdn.com/test1");
+        CommonUserInfo secondCommonUserInfo = CommonUserInfo.make("test2", "https://cdn.com/test2");
+
+        boolean equals = firstCommonUserInfo.equals(secondCommonUserInfo);
+
+        assertSame(equals, false);
+    }
+
+    @Test
+    @DisplayName("수정 시 다른 객체로 인식 테스트")
+    void modify_userinfo() {
+        CommonUserInfo commonUserInfo = CommonUserInfo.make("test", "https://cdn.com/test");
+        CommonUserInfo modifiedCommonUserInfo = commonUserInfo.modifyProfile("https://cdn.com/test2");
+
+        boolean equals = commonUserInfo.equals(modifiedCommonUserInfo);
+
+        assertSame(equals, false);
+    }
+}
