58da10d1bd555b879f40162c40213545ffc99477
0Chord <seaweed.0chord@gmail.com>
2024-05-24 12:44:03 +0900

    test : add channel aggregate unit test

--- src/backend/community-service/src/main/java/harmony/communityservice/guild/channel/domain/Channel.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/guild/channel/domain/Channel.java
@@ -1,5 +1,6 @@
 package harmony.communityservice.guild.channel.domain;
 
+import harmony.communityservice.common.exception.NotFoundDataException;
 import harmony.communityservice.domain.Domain;
 import harmony.communityservice.domain.ValueObject;
 import harmony.communityservice.guild.category.domain.Category.CategoryId;
@@ -26,14 +27,35 @@ public class Channel extends Domain<Channel, ChannelId> {
     public Channel(CategoryId categoryId, ChannelId channelId, GuildId guildId, String name, String type) {
         this.categoryId = categoryId;
         this.channelId = channelId;
+        verifyGuildId(guildId);
         this.guildId = guildId;
+        verifyName(name);
         this.name = name;
+        verifyType(type);
         this.type = ChannelType.valueOf(type);
     }
 
     @Override
     public ChannelId getId() {
         return channelId;
+    }
+
+    private void verifyGuildId(GuildId guildId) {
+        if (guildId == null) {
+            throw new NotFoundDataException("guildId가 존재하지 않습니다");
+        }
+    }
+
+    private void verifyType(String type) {
+        if (type == null) {
+            throw new NotFoundDataException("type이 존재하지 않습니다");
+        }
+    }
+
+    private void verifyName(String name) {
+        if (name == null) {
+            throw new NotFoundDataException("name이 존재하지 않습니다");
+        }
     }
 
     @Getter
