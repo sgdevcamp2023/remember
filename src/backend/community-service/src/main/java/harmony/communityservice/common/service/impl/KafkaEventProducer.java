634bfe337ac8e0ae97a5f498306010c4e6b47253
0Chord <seaweed.0chord@gmail.com>
2024-06-03 02:08:38 +0900

    test : add kafka event producer mock test

--- src/backend/community-service/src/main/java/harmony/communityservice/common/service/impl/KafkaEventProducer.java
+++ src/backend/community-service/src/main/java/harmony/communityservice/common/service/impl/KafkaEventProducer.java
@@ -5,7 +5,9 @@ import harmony.communityservice.common.service.EventProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class KafkaEventProducer implements EventProducer {
 
